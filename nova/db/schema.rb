648abc6dd75d2152251fe3d39991c513d0f22148
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-08 10:33:31 +0900

    emialをuniqueにした

--- nova/db/schema.rb
+++ nova/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_164954) do
+ActiveRecord::Schema.define(version: 2018_05_08_012738) do
 
   create_table "charges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2018_04_23_164954) do
     t.boolean "staff", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "charges", "users"
