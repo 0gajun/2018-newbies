edf41d6abf603671300089c09bea9af12450ae44
Junya Ogasawara <ogasawara.junya@moneyforward.co.jp>
2018-05-10 16:23:48 +0900

    Remove an unused concern, Loginable

--- nova/app/controllers/application_controller.rb
+++ nova/app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  #include Loginable
   protect_from_forgery with: :exception, prepend: true
 
   def after_sign_in_path_for(users)
