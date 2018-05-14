06a924d98832dc8c8d0ffc1a6fbf1a53b248d260
Junya Ogasawara <oga.ivc.s27@gmail.com>
2018-05-15 01:57:00 +0900

    Use environment variable to specify redis and bind addr in pulsar

--- pulsar/cmd/pulsar.go
+++ pulsar/cmd/pulsar.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/0gajun/2018-newbies/pulsar"
 	"github.com/go-redis/redis"
@@ -9,14 +10,32 @@ import (
 
 func main() {
 	redis := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     getRedisAddr(),
 		Password: "",
 		DB:       0,
 	})
 
 	server := pulsar.NewServer(redis)
 
-	if err := server.ListenAndServe("127.0.0.1:3001"); err != nil {
+	bindAddr := getBindAddr()
+
+	log.Printf("Listening on %s", bindAddr)
+
+	if err := server.ListenAndServe(bindAddr); err != nil {
 		log.Fatalf("Failed to listen and server : %v\n", err)
 	}
+}
+
+func getRedisAddr() string {
+	if redisAddr, ok := os.LookupEnv("REDIS_ADDR"); ok {
+		return redisAddr
+	}
+	return "localhost:6379"
+}
+
+func getBindAddr() string {
+	if bindAddr, ok := os.LookupEnv("PULSAR_BIND_ADDR"); ok {
+		return bindAddr
+	}
+	return "127.0.0.1:3001"
 }
