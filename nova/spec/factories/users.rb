6f8693a5e6bb2e98861a167b40e6d94539d9de7c
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-15 11:56:33 +0900

    add amount test

--- nova/spec/factories/users.rb
+++ nova/spec/factories/users.rb
@@ -5,6 +5,5 @@ FactoryBot.define do
     nickname { FFaker::Internet.user_name }
     email { FFaker::Internet.email }
     password { FFaker::Internet.password }
-
   end
 end
--- nova/spec/models/charge_spec.rb
+++ nova/spec/models/charge_spec.rb
@@ -3,26 +3,45 @@
 require 'rails_helper'
 
 RSpec.describe Charge, type: :model do
-  context "when charge request" do
-    let(:stripe_helper) { StripeMock.create_test_helper }
-    before { StripeMock.start }
-    after { StripeMock.stop }
+  describe "#create_stripe_charge" do
+    let(:user) { create(:user) }
 
-    it "retrun error when without credit card" do
-      user = User.create(nickname: 'foo', email: 'baz@foo.com', password: 'foobar', password_confirmation: 'foobar')
-      StripeMock.prepare_card_error(:missing)
-      expect { user.charges.create!(amount: 100) }.to raise_error { |e|
-        expect(e).to be_a ActiveRecord::RecordNotSaved
-        expect(e.message).to eq("Failed to save the record")
-      }
+    context "without credit card" do
+      it "return error" do
+        StripeMock.prepare_card_error(:missing)
+        expect { user.charges.create!(amount: 100) }.to raise_error do |e|
+          expect(e).to be_a ActiveRecord::RecordNotSaved
+          expect(e.message).to eq("Failed to save the record")
+        end
+      end
     end
 
-    it "get stripe_id" do
-      user = User.create(nickname: 'foo', email: 'baz@foo.com', password: 'foobar', password_confirmation: 'foobar')
-      charge = user.charges.create!(amount: 100)
-      expect(charge.stripe_id).not_to eq nil
+    context "with credit card" do
+      it "get stripe_id" do
+        charge = user.charges.create!(amount: 100)
+        expect(charge.stripe_id).not_to eq nil
+      end
+    end
+
+    context "minus amount" do
+      it "return error" do
+        expect { user.charges.create!(amount: -100) }.to raise_error do |e|
+          expect(e).to be_a ActiveRecord::RecordNotSaved
+        end
+      end
+    end
+
+    context "too much amount" do
+      it "return error" do
+        amount = 10_000_000
+        if amount >= 10_000_000
+          custom_error = ActiveRecord::RecordNotSaved.new("too much amount")
+          StripeMock.prepare_error(custom_error, :new_charge)
+          expect { user.charges.create!(amount: amount) }.to raise_error do |e|
+            expect(e).to be_a ActiveRecord::RecordNotSaved
+          end
+        end
+      end
     end
   end
 end
-
-
