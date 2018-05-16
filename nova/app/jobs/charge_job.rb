61ca998b5e0b273e49bcdc31e438bc3a1e6709f4
Goto Shuhei <goto.shuhei@mf-0448-mm01.local>
2018-05-16 12:47:31 +0900

    invalid request & card errorの例外処理対応

--- nova/app/jobs/charge_job.rb
+++ nova/app/jobs/charge_job.rb
@@ -2,25 +2,24 @@ class ChargeJob < ApplicationJob
   queue_as :default
 
   def perform(charge)
+    #raise Stripe::CardError.new("hoge", {}, :missing)
+    #raise Stripe::InvalidRequestError.new("hoge", {})
+
     capture_response = Stripe::Charge.retrieve(charge.stripe_id).capture
     execute!(charge, 'charged')
+
   rescue Stripe::CardError => e
     # Since it's a decline, Stripe::CardError will be caught
     execute!(charge, 'faild')
+
+  rescue Stripe::InvalidRequestError => e
+    # Invalid parameters were supplied to Stripe's API
+    execute!(charge, 'invalid request')
+
   rescue => e
-    body = e.json_body
-    err  = body[:error]
-    puts "Status is: #{e.http_status}"
-    puts "Type is: #{err[:type]}"
-    puts "Charge ID is: #{err[:charge]}"
-    # The following fields are optional
-    puts "Code is: #{err[:code]}" if err[:code]
-    puts "Decline code is: #{err[:decline_code]}" if err[:decline_code]
-    puts "Param is: #{err[:param]}" if err[:param]
-    puts "Message is: #{err[:message]}" if err[:message]
     # automatical retry for sidekiq
-    puts "-----"
-    puts "Retry Job"
+   puts "-----"
+   puts "Retry Job"
   end
 
   def execute!(charge, result) 
@@ -30,10 +29,13 @@ class ChargeJob < ApplicationJob
       else
         return
       end
-      # transactionが終了するとlockは解放される
-      aquire_lock!(user_balance)
 
-      increase_balance!(user_balance, charge.amount)
+      if result == 'charged'
+        # captureが成功した場合balanceを増やす
+        # transactionが終了するとlockは解放される
+        aquire_lock!(user_balance)
+        increase_balance!(user_balance, charge.amount)
+      end
 
       #add charge_history into charge_history table
       ChargeHistory.create!(amount: charge.amount, stripe_id: charge.stripe_id, result: result, user_id: charge.user_id)
