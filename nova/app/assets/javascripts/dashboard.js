f6404f1ad66bd3f6810d8c8053bb3bf2b8d9f0b9
takenaka-kazumasa <takenaka.kazumasa@moneyforward.co.jp>
2018-05-17 10:58:37 +0900

    不要コード削除

--- nova/app/assets/javascripts/dashboard.js
+++ nova/app/assets/javascripts/dashboard.js
@@ -212,11 +212,11 @@ document.addEventListener('DOMContentLoaded', function() {
       updateUser: function(event) {
         if(event) { event.preventDefault(); }
 
-        var self = this;
-        api.put('/users', { user: this.user }).//次root_pathにリダイレクトしてもいいんじゃないか
-          then(function(json) {
-            self.user = json;
-          });
+      //  var self = this;
+      //  api.put('/users', { user: this.user }).//次root_pathにリダイレクトしてもいいんじゃないか
+      //    then(function(json) {
+      //      self.user = json;
+      //    });
       },
     }
   });
--- nova/app/controllers/users/registrations_controller.rb
+++ nova/app/controllers/users/registrations_controller.rb
@@ -2,7 +2,6 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
 
   # GET /users/sign_up
   def new
@@ -46,9 +45,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  # end
 
   # The path used after sign up.
   def after_sign_up_path_for(user)
